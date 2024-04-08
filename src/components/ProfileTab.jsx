9b802402490dfa5d03ecd036796a10e4d9a60dd6
Valood <karataev090903@bk.ru>
2024-04-08 23:47:03 +0500

    api endpoints

--- src/components/ProfileTab.jsx
+++ src/components/ProfileTab.jsx
@@ -21,45 +21,63 @@ const ProfileTab = () => {
         }
     })
 
+    const handleSubmit = (value) => {
+        console.log(value)
+        setIsEdit(false)
+    }
+
     return (
         <Container size='sm' mt='md'>
             {!isEdit ?
-            <Flex direction='column' gap='xs'>
-                <TextInput disabled label='Имя'/>
-                <TextInput disabled label='Город'/>
-                <TextInput disabled label='E-mail'/>
-                <Button
-                    size='md'
-                    mt='md'
-                    onClick={() => setIsEdit(true)}
-                    style={{alignSelf: 'center', width: '40%'}}
-                >Редактировать</Button>
-            </Flex> :
-            <Flex direction='column' gap='xs'>
-                <TextInput
-                    label='Имя'
-                    {...form.getInputProps('name')}
-                />
-                <TextInput
-                    label='Город'
-                    {...form.getInputProps('city')}
-                />
-                <TextInput
-                    label='E-mail'
-                    {...form.getInputProps('email')}
-                />
-                <TextInput
-                    label='Пароль'
-                    type='password'
-                    {...form.getInputProps('password')}
-                />
-                <Button
-                    size='md'
-                    mt='md'
-                    onClick={() => setIsEdit(false)}
-                    style={{alignSelf: 'center', width: '40%'}}
-                >Сохранить изменения</Button>
-            </Flex>}
+                <Flex direction='column' gap='xs'>
+                    <TextInput disabled label='Имя'/>
+                    <TextInput disabled label='Город'/>
+                    <TextInput disabled label='E-mail'/>
+                    <Button
+                        size='md'
+                        mt='md'
+                        onClick={() => setIsEdit(true)}
+                        style={{alignSelf: 'center', width: '40%'}}
+                    >Редактировать</Button>
+                </Flex> :
+                <form onSubmit={form.onSubmit(handleSubmit)}>
+                    <Flex direction='column' gap='xs'>
+                        <TextInput
+                            label='Имя'
+                            {...form.getInputProps('name')}
+                        />
+                        <TextInput
+                            label='Город'
+                            {...form.getInputProps('city')}
+                        />
+                        <TextInput
+                            label='E-mail'
+                            {...form.getInputProps('email')}
+                        />
+                        <TextInput
+                            label='Пароль'
+                            type='password'
+                            {...form.getInputProps('password')}
+                        />
+                        <Flex justify='center' gap='md'>
+                            <Button
+                                type='button'
+                                size='md'
+                                mt='md'
+                                color='red'
+                                style={{width: '40%'}}
+                                onClick={() => setIsEdit(false)}
+                            >Отмена</Button>
+                            <Button
+                                type='submit'
+                                size='md'
+                                mt='md'
+                                style={{width: '40%'}}
+                            >Сохранить изменения</Button>
+                        </Flex>
+                    </Flex>
+                </form>
+            }
         </Container>
     );
 };
--- src/store/api.js
+++ src/store/api.js
@@ -2,9 +2,72 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: ''
+        baseUrl: '',
+        prepareHeaders: (headers) => {
+            const token =  localStorage.getItem('token');
+            if(token){
+                headers.set('Authorization', `Bearer ${token}`)
+            }
+        },
     }),
+    tagTypes: ['PROFILE', 'LECTURE'],
     endpoints: build => ({
-
+        registration: build.mutation({
+            query: body => ({
+                body,
+                url: '/registration',
+                method: 'POST'
+            })
+        }),
+        login: build.mutation({
+            query: body => ({
+                body,
+                url: '/login',
+                method: 'POST'
+            })
+        }),
+        getLectures: build.query({
+            query: () => '/lectures',
+            providesTags: ['LECTURE']
+        }),
+        getLectureContent: build.query({
+            query: id => `/lecture/${id}`
+        }),
+        getUserInfo: build.query({
+            query: () => `/user`,
+            providesTags: ['PROFILE']
+        }),
+        changeProfile: build.mutation({
+            query: body => ({
+                body,
+                url: '/user',
+                method: 'PATCH'
+            }),
+            invalidatesTags: ['PROFILE']
+        }),
+        getUserStatistics: build.query({
+            query: () => `/statistics`
+        }),
+        getOtherStatistics: build.query({
+            query: () => `/statistics/other`
+        }),
+        addLecture: build.mutation({
+            query: body => ({
+                body,
+                url: '/lecture',
+                method: 'POST'
+            }),
+            invalidatesTags: ['LECTURE']
+        }),
+        getTest: build.query({
+            query: () => `/test`
+        }),
+        sendTest: build.mutation({
+            query: body => ({
+                body,
+                url: '/test',
+                method: 'POST'
+            })
+        }),
     })
 })
